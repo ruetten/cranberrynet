24a8cea3ea701967b91e5cc1b98ec853430c2d00
cmvisaya <71780218+cmvisaya@users.noreply.github.com>
2022-10-24 21:30:39 -0500

    some testing with saturation
    generates gray artifacts that still mess with detecting the yellow cranberry - might attempt canny edge detection

--- cranberrynet.py
+++ cranberrynet.py
@@ -9,70 +9,102 @@ def rescaleFrame(frame, scale=0.75):
     dimensions = (width, height)
 
     return cv2.resize(frame, dimensions, interpolation=cv2.INTER_AREA)
-  
+
 # reading image
 img = cv2.imread('Images/BerriesOriginal.png')
 img = rescaleFrame(img, scale=0.5)
-img = cv2.GaussianBlur(img, (7,7), cv2.BORDER_DEFAULT)
-  
+img = cv2.GaussianBlur(img, (15, 15), cv2.BORDER_DEFAULT)
+hsvImg = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+
+#multiple by a factor to change the saturation
+hsvImg[...,1] = hsvImg[...,1]*3
+
+#multiple by a factor of less than 1 to reduce the brightness 
+hsvImg[...,2] = hsvImg[...,2]*1
+
+img=cv2.cvtColor(hsvImg,cv2.COLOR_HSV2BGR)
+
+cv2.imshow('img', img)
+cv2.waitKey(0)
+
+
 # converting image into grayscale image
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-  
+im_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+cv2.imshow('gray', im_gray)
+
+# (thresh, im_bw) = cv2.threshold(im_gray, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+# cv2.imshow('Cranberries', im_bw)
+
+thresh = 60
+im_bw = cv2.threshold(im_gray, thresh, 255, cv2.THRESH_BINARY)[1]
+cv2.imshow('Cranberries', im_bw)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
 # setting threshold of gray image
 thresh_val = 95
-_, threshold = cv2.threshold(gray, thresh_val, 255, cv2.THRESH_BINARY)
-  
+_, threshold = cv2.threshold(im_gray, thresh_val, 255, cv2.THRESH_BINARY)
+
 # using a findContours() function
 contours, _ = cv2.findContours(
     threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-  
+
+# figure out which are cranberries and which are bordering circles
+cranberries = list()
+numberOfBorderCircles = 0
+totalCircleArea = 0
 i = 0
-  
-# list for storing names of shapes
 for contour in contours:
-  
-    # here we are ignoring first counter because 
+    # here we are ignoring first contour because
     # findcontour function detects whole image as shape
     if i == 0:
         i = 1
         continue
-  
+
+    area = cv2.contourArea(contour)
+    # criteria for determining if is border circle or if is cranberry
+    if area >= 2100:
+        numberOfBorderCircles = numberOfBorderCircles + 1
+        totalCircleArea = totalCircleArea + area
+    else:
+        cranberries.append(i)
+    i = i + 1
+
+averageBorderCircleArea = totalCircleArea/numberOfBorderCircles
+
+# list for storing names of shapes
+i = 0
+for contour in contours:
+    area = cv2.contourArea(contour)
+
+    # here we are ignoring first contour because
+    # findcontour function detects whole image as shape
+    if i == 0:
+        i = 1
+        continue
+
     # cv2.approxPloyDP() function to approximate the shape
-    approx = cv2.approxPolyDP(
-        contour, 0.01 * cv2.arcLength(contour, True), True)
-      
+    # approx = cv2.approxPolyDP(
+    #     contour, 0.01 * cv2.arcLength(contour, True), True)
+
     # using drawContours() function
-    cv2.drawContours(img, [contour], 0, (0, 0, 255), 5)
-  
+    cv2.drawContours(img, [contour], 0, (0, 0, 255), 1)
+
     # finding center point of shape
     M = cv2.moments(contour)
     if M['m00'] != 0.0:
         x = int(M['m10']/M['m00'])
         y = int(M['m01']/M['m00'])
-  
-    # putting shape name at center of each shape
-    if len(approx) == 3:
-        cv2.putText(img, 'Triangle', (x, y),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
-  
-    elif len(approx) == 4:
-        cv2.putText(img, 'Quadrilateral', (x, y),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
-  
-    elif len(approx) == 5:
-        cv2.putText(img, 'Pentagon', (x, y),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
-  
-    elif len(approx) == 6:
-        cv2.putText(img, 'Hexagon', (x, y),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
-  
-    else:
-        cv2.putText(img, '', (x, y),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
-  
+
+    if i in cranberries:
+        cv2.putText(img, str(round(area/averageBorderCircleArea, 3)), (x, y),
+                         cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
+    i = i + 1
+
 # displaying the image after drawing contours
 cv2.imshow('Cranberries', img)
-  
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
